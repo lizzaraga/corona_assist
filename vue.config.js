032a398536de64c02cbbf088687506fd820211ae
lizzaraga <fomekongchristmael@gmail.com>
2020-04-19 05:50:47 +0200

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'Corona Assist',
+    themeColor: '#0260FD',
+    msTileColor: '#FCFCFC',
+    manifestOptions: {
+      background_color: '#0260FD'
+    }
+  }
+}
